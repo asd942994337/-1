1bdc9b4b887658e9e3f1248a6c28d33d05c17f0e
yuhaoming <942994337@qq.com>
2024-07-02 17:03:24 +0800

    面试题再看

---
+++ 面试题/算法初试/maopao.js
@@ -0,0 +1,15 @@
+function bubbleSort(arr) {
+    const len = arr.length;
+    for (let i = 0; i < len - 1; i++) {
+        for (let j = 0; j < len - 1 - i; j++) {
+            if (arr[j] > arr[j+1]) {        // 相邻元素两两对比
+                var temp = arr[j+1];        // 元素交换
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    return arr;
+}
+let a = bubbleSort([3,4,2,1,5,6,7,8,9])
+console.log(a);
